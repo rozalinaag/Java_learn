5936d4aa0a0e2d553f29c6ded2ddf65770b4151f
Rozalina <59352861+rozalinaag@users.noreply.github.com>
2021-12-05 18:16:36 +0300

    Создание демон-потока

---
+++ java_book/DaemonThreadApplication.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import javax.swing.*;
+
+import static javax.swing.JOptionPane.*;
+
+public class DaemonThreadApplication {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t = new Thread(()->{ //lambda-expression
+            int k=1, s=0;
+            while(true){
+                System.out.println(Thread.currentThread().getName() + ": " + s);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                s+=k;
+                k++;
+            }
+        }, "Calculating the amounts");
+
+        t.setDaemon(true); //Daemon thread
+        int res;
+        t.start();
+
+        do{
+            Thread.sleep(3000);
+            res = showConfirmDialog(null, "Do you want the end calculating amount?", "Amount of numbers", JOptionPane.YES_NO_CANCEL_OPTION);
+        } while(res!=YES_OPTION);
+    }
+}
